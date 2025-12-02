229d81197639250231b7d056900f312f7d69f5b8
Dhan <edan.belgica@cvsu.edu.ph>
2025-12-02 11:25:24 +0800

    refractor: password change

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const SUPABASE_ANON_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNzbndoaW1iZmNxbnltanVnc29tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQxNDc1NzksImV4cCI6MjA3OTcyMzU3OX0.exwo5xPHyjm7K913k9lMP75w-BZabXlDAWLNWHkE2y0";
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-const DELETE_PASSWORD = "secret123";
+const DELETE_PASSWORD = "LucasLiam1725";
 
 export default function App() {
   const [images, setImages] = useState([]);
